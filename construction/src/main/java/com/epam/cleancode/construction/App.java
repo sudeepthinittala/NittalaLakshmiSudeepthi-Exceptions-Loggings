7ba165c058f5ac2d3ce380f191074025dc696c5c
sudeepthinittala <sudeepthinittala@gmail.com>
2020-02-24 11:42:48 +0530

    firstcommit

--- construction/src/main/java/com/epam/cleancode/construction/App.java
+++ construction/src/main/java/com/epam/cleancode/construction/App.java
@@ -2,26 +2,33 @@ package com.epam.cleancode.construction;
 
 import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static final Logger LOGGER=LogManager.getLogger(App.class);
     public static void main( String[] args )
     {
-    		Scanner sc=new Scanner(System.in);
+    		@SuppressWarnings("resource")
+			Scanner sc=new Scanner(System.in);
     		SimpleInterest s=new SimpleInterest();
     		s.getValues();
-    		System.out.println(s.CalculateSimpleInterest());
+    		LOGGER.info(s.CalculateSimpleInterest());
     		CompoundInterest c=new CompoundInterest();
     		c.getValues();
-    		System.out.println(c.CalculateCompoundInterest());
+    		LOGGER.info(c.CalculateCompoundInterest());
     		Construction con=new Construction();
-    		System.out.println("Enter Area of the house");
+    		LOGGER.info("Enter Area of the house");
     		float area=sc.nextFloat();
-    		System.out.println("Enter the type of material\n1.Standard \n2.Above Standard\n3.High Standard Materials\n4.High Standard Material and Fully Automated Home");
+    		LOGGER.info("Enter the type of material\n1.Standard \n2.Above Standard\n3.High Standard Materials\n4.High Standard Material and Fully Automated Home");
     		int flag=sc.nextInt();
-    		System.out.println("Cost for Construction is:"+con.construct(flag,area));
+    		LOGGER.info("Cost for Construction is:"+con.construct(flag,area));
+    		
     }
 }
